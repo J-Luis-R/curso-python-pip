7d34a769bdf0d5d8ea5c356142c7e32a78bf651a
JLuis <pepelu0285@gmail.com>
2026-04-14 23:23:50 -0600

    web-server actualizado

--- web-server/store.py
+++ web-server/store.py
@@ -3,8 +3,12 @@ import requests
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
     print(r.status_code)
+    print("-"*40)
     print(r.text)
+    print("-"*40)
     print(type(r.text))
+    print("-"*40)
     categories = r.json()
     for category in categories:
         print(category['name'])
+        print("-"*40)
